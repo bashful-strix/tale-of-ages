fb2340e2aa6fa77c8a411f5125375d91beee3472
bashful-strix <19427075+bashful-strix@users.noreply.github.com>
2025-10-15 17:40:42 +0100

    Set up halogen and purs deps

--- src/main.purs
+++ src/main.purs
@@ -5,7 +5,21 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+import Halogen (defaultEval, mkComponent, mkEval)
+import Halogen.Aff (awaitBody, runHalogenAff)
+import Halogen.HTML as H
+import Halogen.VDom.Driver (runUI)
+
 main :: Effect Unit
 main = do
   log "🍝"
+  runHalogenAff $
+    runUI component unit =<< awaitBody
 
+  where
+  component =
+    mkComponent
+      { initialState: const unit
+      , render: const $ H.text "init"
+      , eval: mkEval defaultEval
+      }
