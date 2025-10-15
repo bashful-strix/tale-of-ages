e9843e7b2d0c393b9d17c782229a114555f84ae7
bashful-strix <19427075+bashful-strix@users.noreply.github.com>
2025-10-15 17:40:42 +0100

    Set up deku and purs deps

--- src/main.purs
+++ src/main.purs
@@ -5,7 +5,10 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+import Deku.DOM as D
+import Deku.Toplevel (runInBody)
+
 main :: Effect Unit
 main = do
   log "🍝"
-
+  void $ runInBody $ D.text_ "init"
