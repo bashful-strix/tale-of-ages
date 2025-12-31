47f9e6e25eb48b148909f538806841ae9362b887
bashful-strix <19427075+bashful-strix@users.noreply.github.com>
2025-12-31 11:41:22 +0000

    Add Knave job

--- src/resource/icon/job/knave.purs
+++ src/resource/icon/job/knave.purs
@@ -3,18 +3,54 @@ module ToA.Resource.Icon.Job.Knave
   ) where
 
 import Data.FastVect.FastVect ((:), empty)
+import Data.Maybe (Maybe(..))
 import Data.Tuple.Nested ((/\))
 
 import ToA.Data.Icon (Icon)
+import ToA.Data.Icon.Ability
+  ( Ability(..)
+  , Action(..)
+  , Range(..)
+  , Step(..)
+  , StepType(..)
+  , Tag(..)
+  , Variable(..)
+  )
+import ToA.Data.Icon.Dice (Die(..))
 import ToA.Data.Icon.Job (Job(..), JobLevel(..))
+import ToA.Data.Icon.Keyword (Keyword(..), Category(..), StatusType(..))
 import ToA.Data.Icon.Markup (MarkupItem(..))
 import ToA.Data.Icon.Name (Name(..))
+import ToA.Data.Icon.Talent (Talent(..))
+import ToA.Data.Icon.Trait (Trait(..))
 
 knave :: Icon
 knave =
   { classes: []
   , colours: []
   , souls: []
+  , foes: []
+  , foeClasses: []
+  , factions: []
+
+  , keywords:
+      [ Keyword
+          { name: Name "Hatred"
+          , category: Status Negative
+          , description:
+              [ Text
+                  """Hatred is a unique negative status. A character suffering
+                  from hatred deals 1/2 dmaage to all characters other than you
+                  as long as you are not immune """
+              , Italic [ Ref (Name "Immune") [ Text "immune" ] ]
+              , Text
+                  """ to damage, and in line of sight and range 1-3 from them. A
+                  characters removed one hatred after using any ability that
+                  damages you."""
+              ]
+          }
+      ]
+
   , jobs:
       [ Job
           { name: Name "Knave"
@@ -56,15 +92,204 @@ knave =
           , talents:
               Name "Brawl"
                 : Name "Suffer"
-                : Name "Opress"
+                : Name "Oppress"
                 : empty
           }
       ]
-  , traits: []
-  , talents: []
-  , abilities: []
-  , keywords: []
-  , foes: []
-  , foeClasses: []
-  , factions: []
+
+  , traits:
+      [ Trait
+          { name: Name "Hatred"
+          , description:
+              [ Text
+                  """Once a round, when you damage a target, you may inflict
+                  them with """
+              , Italic [ Ref (Name "Hatred") [ Text "hatred (-)" ] ]
+              , Text
+                  """. Hatred is a unique negative status. A character suffering
+                  from hatred deals 1/2 dmaage to all characters other than you
+                  as long as you are not immune """
+              , Italic [ Ref (Name "Immune") [ Text "immune" ] ]
+              , Text
+                  """ to damage, and in line of sight and range 1-3 from them. A
+                  characters removed one hatred after using any ability that
+                  damages you."""
+              ]
+          , subItem: Nothing
+          }
+      ]
+
+  , talents:
+      [ Talent
+          { name: Name "Brawl"
+          , colour: Name "Red"
+          , description:
+              [ Text "Improve the effect of "
+              , Italic [ Ref (Name "Daze") [ Text "dazed" ] ]
+              , Text
+                  """ to -3 damage on attacks against you and adjacent allies.
+                  When you critical hit, you may inflict """
+              , Italic [ Text "dazed" ]
+              , Text "."
+              ]
+          , subItem: Nothing
+          }
+      , Talent
+          { name: Name "Suffer"
+          , colour: Name "Red"
+          , description:
+              [ Text
+                  """Once a round, when a foe in range 1-3 is defeated, you may
+                  transfer all stacks of one of its negatice statuses to any
+                  other foe in range 1-3."""
+              ]
+          , subItem: Nothing
+          }
+      , Talent
+          { name: Name "Oppress"
+          , colour: Name "Red"
+          , description:
+              [ Text
+                  """When you hit an attack against a foe, you may increase one
+                  of its statuses by +1 after the attack resolves, or +2 if that
+                  foe was in crisis."""
+              ]
+          , subItem: Nothing
+          }
+      ]
+
+  , abilities:
+      [ LimitBreak
+          { name: Name "Mock"
+          , colour: Name "Red"
+          , description:
+              [ Text
+                  """There are innumerable words in innumerable tongues for what
+                  I am about to say to you. Fortunately, I have picked the very
+                  best."""
+              ]
+          , cost: One /\ 3
+          , tags: [ RangeTag (Range (NumVar 1) (NumVar 5)) ]
+          , steps:
+              [ Step Eff Nothing
+                  [ Text "Target foe takes "
+                  , Dice 1 D3
+                  , Text " "
+                  , Italic [ Ref (Name "Pierce") [ Text "piercing" ] ]
+                  , Text " damage, then gains 3 "
+                  , Italic [ Ref (Name "Daze") [ Text "dazed" ] ]
+                  , Text ", 3 "
+                  , Italic [ Ref (Name "Stun") [ Text "stun" ] ]
+                  , Text ", and becomes "
+                  , Italic [ Ref (Name "Immobile") [ Text "immobile" ] ]
+                  , Text
+                      """ and unable to take interrupts until the start of its
+                      next turn."""
+                  ]
+              , Step Eff Nothing
+                  [ Text
+                      """If you defeat a legend or elite foe with this ability,
+                      immediately gain 1 personal resolve or grant 1 to an
+                      ally."""
+                  ]
+              ]
+          }
+      , Ability
+          { name: Name "Low Blow"
+          , colour: Name "Red"
+          , description: [ Text "Hit them right in the gronch." ]
+          , cost: One
+          , tags: [ Attack ]
+          , steps:
+              [ Step Eff Nothing [ Text "Dash 1." ]
+              , AttackStep [ Text "2 damage" ] [ Text "+", Dice 1 D3 ]
+              , Step (KeywordStep (Name "Afflicted")) Nothing
+                  [ Text
+                      """Deal 2 damage again. If your target has 3 or more
+                      negative status tokens of any kind, deal 4 damage again
+                      instead."""
+                  ]
+              ]
+          }
+      , Ability
+          { name: Name "Taunt"
+          , colour: Name "Red"
+          , description:
+              [ Text
+                  """With well places words, you rile up your foe into moving
+                  out of position."""
+              ]
+          , cost: One
+          , tags: [ RangeTag (Range (NumVar 1) (NumVar 4)) ]
+          , steps:
+              [ Step Eff (Just D3)
+                  [ Text "Gain "
+                  , Italic [ Ref (Name "Shield") [ Text "shield" ] ]
+                  , Text
+                      """. A foe in range is pulled 1, then must save or be
+                      pulled +"""
+                  , Dice 1 D3
+                  , Text " spaces, become "
+                  , Italic [ Ref (Name "Immobile") [ Text "immobile" ] ]
+                  , Text
+                      """, and become unable to take interrupts. These effects
+                      last until the start of theur next turn."""
+                  ]
+              , Step (KeywordStep (Name "Afflicted")) Nothing
+                  [ Text "Foes take 2 "
+                  , Italic [ Ref (Name "Pierce") [ Text "piercing" ] ]
+                  , Text " damage, or "
+                  , Dice 2 D3
+                  , Text " if they are affilcted by 3 or more statuses."
+                  ]
+              ]
+          }
+      , Ability
+          { name: Name "Sucker Punch"
+          , colour: Name "Red"
+          , description:
+              [ Text
+                  """There's nothing that can't be solved with the liberal
+                  application of fists to faces."""
+              ]
+          , cost: Interrupt (NumVar 1)
+          , tags: [ RangeTag (Range (NumVar 1) (NumVar 2)) ]
+          , steps:
+              [ Step TriggerStep Nothing
+                  [ Text
+                      "An enemy in range rolls a save and you see the result."
+                  ]
+              , Step Eff Nothing
+                  [ Text
+                      """The enemy must re-roll the save keeping the second
+                      result. If they fail the save, you may gain 2 vigor or
+                      clear a negative status."""
+                  ]
+              , Step (KeywordStep (Name "Afflicted")) Nothing
+                  [ Text "Gain one more use of this interrupt this round." ]
+              ]
+          }
+      , Ability
+          { name: Name "Misericorde"
+          , colour: Name "Red"
+          , description: [ Text "Put them out of their misery." ]
+          , cost: Two
+          , tags: [ Attack, RangeTag Melee ]
+          , steps:
+              [ AttackStep [ Text "2 damage" ] [ Text "+", Dice 2 D6 ]
+              , Step OnHit Nothing
+                  [ Text
+                      """Deal +1 damage for each separate type of negative
+                      status token your foe is suffering from (max +6)."""
+                  ]
+              , Step Eff Nothing
+                  [ Text "If your foe is in "
+                  , Italic [ Ref (Name "Crisis") [ Text "crisis" ] ]
+                  , Text ", also destroy "
+                  , Dice 2 D6
+                  , Text " vigor on the target before dealing damage."
+                  ]
+              ]
+          }
+      ]
   }
