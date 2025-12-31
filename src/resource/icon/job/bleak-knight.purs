152a2a47df6a7dac7b5c875df395a59fe3859f53
bashful-strix <19427075+bashful-strix@users.noreply.github.com>
2025-12-31 12:53:04 +0000

    Add Bleak Knight job

--- src/resource/icon/job/bleak-knight.purs
+++ src/resource/icon/job/bleak-knight.purs
@@ -3,18 +3,36 @@ module ToA.Resource.Icon.Job.BleakKnight
   ) where
 
 import Data.FastVect.FastVect ((:), empty)
+import Data.Maybe (Maybe(..))
 import Data.Tuple.Nested ((/\))
 
 import ToA.Data.Icon (Icon)
+import ToA.Data.Icon.Ability
+  ( Ability(..)
+  , Action(..)
+  , Range(..)
+  , Step(..)
+  , StepType(..)
+  , Tag(..)
+  , Variable(..)
+  )
+import ToA.Data.Icon.Dice (Die(..))
 import ToA.Data.Icon.Job (Job(..), JobLevel(..))
-import ToA.Data.Icon.Markup (MarkupItem(..))
+import ToA.Data.Icon.Markup (MarkupItem(..), ListKind(..))
 import ToA.Data.Icon.Name (Name(..))
+import ToA.Data.Icon.Talent (Talent(..))
+import ToA.Data.Icon.Trait (Trait(..))
 
 bleakKnight :: Icon
 bleakKnight =
   { classes: []
   , colours: []
   , souls: []
+  , keywords: []
+  , foes: []
+  , foeClasses: []
+  , factions: []
+
   , jobs:
       [ Job
           { name: Name "Bleak Knight"
@@ -58,11 +76,211 @@ bleakKnight =
                 : empty
           }
       ]
-  , traits: []
-  , talents: []
-  , abilities: []
-  , keywords: []
-  , foes: []
-  , foeClasses: []
-  , factions: []
+
+  , traits:
+      [ Trait
+          { name: Name "Armored Agony"
+          , description:
+              [ List Unordered
+                  [ [ Text
+                        """When you inflict a status on a foe, you can also
+                        inflict it on yourself."""
+                    ]
+                  , [ Text
+                        """You can choose not to clear negative status tokens
+                        after triggering them."""
+                    ]
+                  , [ Text
+                        """When you are afflicted by 3 or more negative status
+                        tokens of any kind, you gain +1 armor and your attacks
+                        deal +2 base damage."""
+                    ]
+                  ]
+              ]
+          , subItem: Nothing
+          }
+      ]
+
+  , talents:
+      [ Talent
+          { name: Name "Tenacity"
+          , colour: Name "Red"
+          , description:
+              [ Text
+                  """If you have 3 or more negative status tokens, you may
+                  choose to clear them at the end of your turn, and gain 2
+                  vigor. Increase this vigor by +"""
+              , Dice 1 D6
+              , Text " if you're in crisis."
+              ]
+          , subItem: Nothing
+          }
+      , Talent
+          { name: Name "Torment"
+          , colour: Name "Red"
+          , description:
+              [ Text
+                  """Adjacent foes do not clear negative statuses. Foes in
+                  crisis additionally cannot gain vigor."""
+              ]
+          , subItem: Nothing
+          }
+      , Talent
+          { name: Name "Sanguine"
+          , colour: Name "Red"
+          , description:
+              [ Text "You gain "
+              , Power
+              , Text
+                  """ on saves if you are afflicted. If you have 3 or more
+                  negative statuses, you automatically pass saves."""
+              ]
+          , subItem: Nothing
+          }
+      ]
+
+  , abilities:
+      [ LimitBreak
+          { name: Name "Chain of Misery"
+          , colour: Name "Red"
+          , description:
+              [ Text
+                  """You link your soul aether with an ally's, intertwining your
+                  suffering, and bolstering their resolve with your own
+                  boundless tolerance for pain."""
+              ]
+          , cost: One /\ 2
+          , tags:
+              [ KeywordTag (Name "Mark")
+              , RangeTag (Range (NumVar 1) (NumVar 3))
+              ]
+          , steps:
+              [ Step (KeywordStep (Name "Mark")) Nothing
+                  [ Text
+                      """You bond yourself to an ally in range. As long as that
+                      ally is in range, they take 1/2 damage while the bond is
+                      active, but you """
+                  , Italic [ Ref (Name "Sacrifice") [ Text "sacrifice" ] ]
+                  , Text
+                      """ hp equal to the other half, before reductions. Any
+                      statuses inflicted on that ally are inflicted on you
+                      instead."""
+                  , Newline
+                  , Text
+                      """If a bonded character is bloodied, both characters deal
+                      +2 damage on hit with their attack, increased to +3 if a
+                      character is in crisis. The bond snaps and ends if either
+                      character is defeated."""
+                  ]
+              ]
+          }
+      , Ability
+          { name: Name "Demise"
+          , colour: Name "Red"
+          , description: [ Text "Pain within becomes pain without." ]
+          , cost: One
+          , tags: [ Attack, RangeTag (Range (NumVar 1) (NumVar 2)) ]
+          , steps:
+              [ Step Eff Nothing
+                  [ Text
+                      """If you are suffering from 3 negative status tokens of
+                      any kind, this attack gains """
+                  , Italic [ Ref (Name "Pierce") [ Text "pierce" ] ]
+                  , Text "."
+                  ]
+              , AttackStep [ Text "2 damage" ] [ Text "+", Dice 2 D3 ]
+              , Step Eff (Just D3)
+                  [ Text "Inflict "
+                  , Dice 1 D3
+                  , Text " "
+                  , Italic [ Ref (Name "Daze") [ Text "daze" ] ]
+                  , Text " on yourself."
+                  ]
+              ]
+          }
+      , Ability
+          { name: Name "Masochism"
+          , colour: Name "Red"
+          , description:
+              [ Text
+                  """Your aether reaches out to your allies, spreading their
+                  suffering to you and lifting their plight."""
+              ]
+          , cost: One
+          , tags: [ RangeTag (Range (NumVar 1) (NumVar 2)) ]
+          , steps:
+              [ Step Eff Nothing
+                  [ Text
+                      """Transfer all negative statuses from an alliy in range
+                      to you, then gain 1 vigor per status token transferred.
+                      If you're blooedied, you may repeat this effect on a
+                      second ally, If you're in crisis, you may repeat on """
+                  , Italic [ Text "all" ]
+                  , Text " allies in range."
+                  ]
+              ]
+          }
+      , Ability
+          { name: Name "Revenge"
+          , colour: Name "Red"
+          , description:
+              [ Text
+                  """You take stock of any slight, letting it fuel a terrible
+                  fire in your gut."""
+              ]
+          , cost: One
+          , tags:
+              [ KeywordTag (Name "Stance")
+              , KeywordTag (Name "Power Die")
+              , KeywordTag (Name "Aura")
+              ]
+          , steps:
+              [ Step (KeywordStep (Name "Stance")) Nothing
+                  [ Text
+                      """While in this stance, gain aura 1. Set out a power die,
+                      or tick that die up by 1 when:"""
+                  , List Unordered
+                      [ [ Text "an ally in the aura gains a status" ]
+                      , [ Text "an ally in the aura takes damage" ]
+                      , [ Text
+                            """increase ticks from each effect + 1 if the ally
+                            was bloodied"""
+                        ]
+                      ]
+                  , Text
+                      """Each condition can only trigger once a round. When the
+                      die is 4 or higher, you become enraged and powered up. You
+                      and allies in the aura take 1/2 damage and become """
+                  , Italic [ Ref (Name "Unstoppable") [ Text "unstoppable" ] ]
+                  , Text
+                      ". Then exit this stance at the start of your next turn."
+                  ]
+              ]
+          }
+      , Ability
+          { name: Name "Quietus"
+          , colour: Name "Red"
+          , description:
+              [ Text "Dark eather coats your blade, cutting the soul." ]
+          , cost: Two
+          , tags: [ Attack, RangeTag Melee ]
+          , steps:
+              [ AttackStep
+                  [ Text "2 damage" ]
+                  [ Text "Foe must also "
+                  , Italic [ Ref (Name "Sacrifice") [ Text "sacrifice" ] ]
+                  , Text " "
+                  , Dice 2 D3
+                  , Text
+                      """+2 hp. Grant half as mush as the sacrifice amount as
+                      vigor to self and adjacent allies to your foe."""
+                  ]
+              , Step Eff Nothing
+                  [ Text "Inflict "
+                  , Italic [ Ref (Name "Stun") [ Text "stun" ] ]
+                  , Text " on yourself. Do not clear this status this turn."
+                  ]
+              ]
+          }
+      ]
   }
