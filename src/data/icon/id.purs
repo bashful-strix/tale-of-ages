0571a9a6e0060edc5cc7e73b67b42c599f12f708
bashful-strix <19427075+bashful-strix@users.noreply.github.com>
2026-01-25 12:52:22 +0000

    Add ID class

---
+++ src/data/icon/id.purs
@@ -0,0 +1,24 @@
+module ToA.Data.Icon.Id
+  ( Id(..)
+  , class Identified
+  , _id
+  , jsonId
+  ) where
+
+import Prelude
+
+import Data.Codec.JSON (Codec, coercible, string)
+import Data.Lens (Lens')
+import Data.Newtype (class Newtype)
+
+newtype Id = Id String
+
+derive instance Newtype Id _
+derive newtype instance Eq Id
+derive newtype instance Ord Id
+
+class Identified a where
+  _id :: Lens' a Id
+
+jsonId :: Codec Id
+jsonId = coercible "Id" string
