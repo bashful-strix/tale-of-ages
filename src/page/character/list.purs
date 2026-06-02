83c37a74f764457623a22b23e10cc43607941e71
bashful-strix <19427075+bashful-strix@users.noreply.github.com>
2026-06-02 21:24:09 +0100

    [feat:src] Add export links to character list

--- src/page/character/list.purs
+++ src/page/character/list.purs
@@ -6,6 +6,7 @@ import Prelude
 
 import CSS (backgroundColor, render, renderedInline)
 
+import Data.Codec (encode)
 import Data.Foldable (foldMap, intercalate)
 import Data.Lens
   ( (^.)
@@ -22,7 +23,7 @@ import Data.Lens
 import Data.Lens.Common (simple)
 import Data.Lens.Indexed (itraversed)
 import Data.Lens.Iso.Newtype (_Newtype)
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Data.Tuple.Nested ((/\))
 
 import Deku.Core (Nut)
@@ -31,10 +32,11 @@ import Deku.DOM.Attributes as DA
 import Deku.DOM.Listeners as DL
 import Deku.Hooks ((<#~>))
 
+import JSURI (encodeURIComponent)
 import Routing.Duplex (print)
 
 import ToA.Data.Env (Env, _navigate)
-import ToA.Data.Icon.Character (_build, _jobs, _level, _primary)
+import ToA.Data.Icon.Character (stringCharacter, _build, _jobs, _level, _primary)
 import ToA.Data.Icon.Colour (_colour, _value)
 import ToA.Data.Icon.Name (_name)
 import ToA.Data.Icon.Sign (_sign)
@@ -44,7 +46,7 @@ import ToA.Util.Optic ((^::))
 
 listCharacterPage :: Env -> Nut
 listCharacterPage env@{ characters, icon } =
-  ((/\) <$> characters <*> icon) <#~> \(chars /\ { colours, jobs }) ->
+  ((/\) <$> characters <*> icon) <#~> \(chars /\ icon_@{ colours, jobs }) ->
     D.div
       [ css_ [ "flex", "flex-col", "items-center", "grow", "gap-2" ] ]
       [ chars # foldMap \char ->
@@ -52,75 +54,99 @@ listCharacterPage env@{ characters, icon } =
             primary = char ^. _build <<< _primary
             job = jobs # traversed `findOf` (view _name >>> eq primary)
           in
-            D.a
+            D.div
               [ css_
                   [ "flex"
-                  , "items-center"
-                  , "gap-2"
                   , "w-full"
                   , "sm:max-w-4/5"
                   , "md:max-w-3/5"
-                  , "p-2"
                   , "rounded"
                   , "bg-stone-500"
                   , "text-stone-800"
-                  , "dark:bg-stone-700"
-                  , "dark:text-stone-300"
-                  , "hover:bg-stone-400"
-                  , "focus:bg-stone-400"
-                  , "dark:hover:bg-stone-500"
-                  , "dark:focus:bg-stone-500"
                   ]
-              , DA.href_ $ print routeCodec $ Characters $ Just $ ViewChar $
-                  char
-                    ^. _name
-              , DL.click_ $
-                  (env ^. _navigate)
-                    (Characters $ Just $ ViewChar $ char ^. _name) <<< pure
               ]
-              [ D.div
-                  [ css_ $ [ "size-16" ]
-                      <> job ^:: _Just <<< _sign <<< _Newtype
-                  ]
-                  []
-              , D.div
-                  [ css_ [ "flex", "flex-col" ] ]
-                  [ D.h3
-                      [ css_ [ "font-bold" ] ]
-                      [ D.text_ $ char ^. _name <<< _Newtype ]
-
-                  , D.div
-                      [ css_ [ "flex", "gap-2" ] ]
-                      [ D.div
-                          []
-                          [ D.text_ $ "L " <> char
-                              ^. _build <<< _level <<< to show
-                          ]
-                      , D.text_ "∷"
-                      , D.div
-                          [ css_ [ "font-bold", "text-white" ]
-                          , DA.style_ $ fromMaybe "" $ renderedInline $
-                              render =<< colours
-                                ^? traversed
-                                  <<< filtered
-                                    ( preview _name >>> eq
-                                        (job ^? _Just <<< _colour)
-                                    )
-                                  <<< _value
-                                  <<< to backgroundColor
-                          ]
-                          [ D.text_ $ primary ^. simple _Newtype ]
+              [ D.a
+                  [ css_
+                      [ "flex"
+                      , "grow"
+                      , "items-center"
+                      , "gap-2"
+                      , "p-2"
+                      , "dark:bg-stone-700"
+                      , "dark:text-stone-300"
+                      , "hover:bg-stone-400"
+                      , "focus:bg-stone-400"
+                      , "dark:hover:bg-stone-500"
+                      , "dark:focus:bg-stone-500"
                       ]
-
-                  , D.div
+                  , DA.href_ $ print routeCodec $ Characters $ Just $ ViewChar $
+                      char ^. _name
+                  , DL.click_ $
+                      (env ^. _navigate)
+                        (Characters $ Just $ ViewChar $ char ^. _name) <<< pure
+                  ]
+                  [ D.div
+                      [ css_ $ [ "size-16" ]
+                          <> job ^:: _Just <<< _sign <<< _Newtype
+                      ]
                       []
-                      [ D.text_
-                          $ intercalate " | "
-                          $ char # (_build <<< _jobs <<< itraversed)
-                              `ifoldMapOf` \n l ->
-                                [ (n ^. simple _Newtype) <> " " <> show l ]
+                  , D.div
+                      [ css_ [ "flex", "flex-col" ] ]
+                      [ D.h3
+                          [ css_ [ "font-bold" ] ]
+                          [ D.text_ $ char ^. _name <<< _Newtype ]
+
+                      , D.div
+                          [ css_ [ "flex", "gap-2" ] ]
+                          [ D.div []
+                              [ D.text_ $ "L " <> char
+                                  ^. _build <<< _level <<< to show
+                              ]
+                          , D.text_ "∷"
+                          , D.div
+                              [ css_ [ "font-bold", "text-white" ]
+                              , DA.style_ $ fromMaybe "" $ renderedInline $
+                                  render =<< colours
+                                    ^? traversed
+                                      <<< filtered
+                                        ( preview _name >>> eq
+                                            (job ^? _Just <<< _colour)
+                                        )
+                                      <<< _value
+                                      <<< to backgroundColor
+                              ]
+                              [ D.text_ $ primary ^. simple _Newtype ]
+                          ]
+
+                      , D.div []
+                          [ D.text_
+                              $ intercalate " | "
+                              $ char # (_build <<< _jobs <<< itraversed)
+                                  `ifoldMapOf` \n l ->
+                                    [ (n ^. simple _Newtype) <> " " <> show l ]
+                          ]
                       ]
                   ]
+
+              , (encodeURIComponent $ encode (stringCharacter icon_) char)
+                  # maybe mempty \charData ->
+                    D.a
+                      [ css_
+                          [ "flex"
+                          , "items-center"
+                          , "gap-2"
+                          , "p-2"
+                          , "dark:bg-stone-700"
+                          , "dark:text-stone-300"
+                          , "hover:bg-stone-400"
+                          , "focus:bg-stone-400"
+                          , "dark:hover:bg-stone-500"
+                          , "dark:focus:bg-stone-500"
+                          ]
+                      , DA.href_ $ "data:text/plain;charset=utf8," <> charData
+                      , DA.download_ $ (char ^. _name <<< _Newtype) <> ".txt"
+                      ]
+                      [ D.text_ "Export" ]
               ]
 
       , D.a
